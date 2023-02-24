f65d3f24b97116ea5387155460498cd3266871bf
data-miner00 <data-miner00@users.noreply.github.com>
2023-02-24 21:28:23 +0800

    feat: Esbuild css bundle

--- src/index.fs
+++ src/index.fs
@@ -2,7 +2,10 @@
 
 open Feliz
 open Browser.Dom
+open Fable.Core.JsInterop
 
 module Index =
+    importDynamic "./App.css" |> fun x -> x.``then``(fun x -> printfn "App.css loaded") |> ignore
+
     let root = ReactDOM.createRoot(document.getElementById "app")
     root.render(Components.HelloWorld())
