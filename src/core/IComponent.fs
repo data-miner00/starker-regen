1e6ee2b1ba94e632569883953cb7fae759a30cd2
data-miner00 <data-miner00@users.noreply.github.com>
2023-02-25 15:17:54 +0800

    docs: Add docstring

--- src/core/IComponent.fs
+++ src/core/IComponent.fs
@@ -2,5 +2,12 @@
 
 open Fable.React
 
+/// <summary>
+/// An interface that all React components must implement.
+/// </summary>
 type IComponent =
+
+    /// <summary>
+    /// The method that returns a <see cref="ReactElement"/>.
+    /// </summary>
     abstract member make : unit -> ReactElement
