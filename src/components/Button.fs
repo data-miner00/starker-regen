bafa3790e543e12f917784bc663df764aadb4b54
data-miner00 <data-miner00@users.noreply.github.com>
2023-02-25 15:21:20 +0800

    feat: Working component inhering IComponent

---
+++ src/components/Button.fs
@@ -0,0 +1,14 @@
+﻿namespace App.Components
+
+open App.Core
+open Feliz
+
+type Button() =
+    interface IComponent with
+        [<ReactComponent>]
+        member this.make() = Html.button [
+            prop.className [ "border border-solid border-blue-100 text-xl font-bold bg-gray-200 px-3 py-1 rounded-lg hover:bg-gray-300" ]
+            prop.text "My button"
+        ]
+
+    member this.make() = (this :> IComponent).make()
--- src/index.fs
+++ src/index.fs
@@ -3,9 +3,11 @@
 open Feliz
 open Browser.Dom
 open Fable.Core.JsInterop
+open App.Components
 
 module Index =
     importDynamic "./App.css" |> fun x -> x.``then``(fun x -> printfn "App.css loaded") |> ignore
 
     let root = ReactDOM.createRoot(document.getElementById "app")
-    root.render(Components.HelloWorld())
+
+    root.render(Button().make())
