103bbfd3dd8d696ff89c0e2810c9d196ca57d55d
data-miner00 <data-miner00@users.noreply.github.com>
2023-02-25 23:08:47 +0800

    feat: Add routing

--- src/App.fs
+++ src/App.fs
@@ -1,13 +1,22 @@
 ﻿namespace App
 
 open Feliz
+open Feliz.Router
+open App.Components
+open App.Pages
+open Fable.Core.JsInterop
 
-type Components =
-    /// <summary>
-    /// A simple component.
-    /// </summary>
+module App =
     [<ReactComponent>]
-    static member HelloWorld() = Html.h1 [
-        prop.className [ "text-green-500" ]
-        prop.text "Hello, world"
-    ]
+    let Make() =
+        let (currentUrl, updateUrl) = React.useState(Router.currentUrl())
+
+        React.router [
+            router.onUrlChanged updateUrl
+            router.children [
+                match currentUrl with
+                | [ ] -> Landing.Make()
+                | [ "button" ] -> Button().make()
+                | _ -> Html.h1 "Not found"
+            ]
+        ]
