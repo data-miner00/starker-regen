b024110511e9d4ec0b359d45507d61662db89071
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-11 20:00:39 +0800

    wip: Added routing module

---
+++ src/utils/Routing.fs
@@ -0,0 +1,18 @@
+﻿namespace App.Utils
+
+open Feliz.Router
+open Browser.Types
+
+module Routing =
+    type State = { CurrentUrl: string list }
+
+    type Msg = NavigateTo of string
+
+    let update msg state =
+        match msg with
+        | NavigateTo href -> state, Cmd.navigatePath href
+
+    let goToUrl (dispatch: Msg -> unit) (href: string) (e: MouseEvent) =
+        e.preventDefault()
+
+        dispatch (NavigateTo href)
